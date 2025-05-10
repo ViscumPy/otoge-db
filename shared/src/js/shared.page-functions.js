67428c192a776b5c5233bb55fe41e5ab2c1e1c54
seyana <93588398+Norca0721@users.noreply.github.com>
2025-05-10 16:49:33 +0800

    fix undefined

--- shared/src/js/shared.page-functions.js
+++ shared/src/js/shared.page-functions.js
@@ -17,90 +17,90 @@ let scrollPos;
 // }
 
 document.addEventListener('DOMContentLoaded', function () {
-  function applyColorScheme() {
-    const selectedScheme = document.querySelector('input[name="colorScheme"]:checked').value;
+    function applyColorScheme() {
+        const selectedScheme = document.querySelector('input[name="colorScheme"]:checked').value;
 
-    // Save the selected color scheme in localStorage
-    localStorage.setItem('userColorScheme', selectedScheme);
+        // Save the selected color scheme in localStorage
+        localStorage.setItem('userColorScheme', selectedScheme);
 
-    // Apply the selected color scheme with a transition effect
-    // root.style.transition = 'background-color 0.5s';
-    root.setAttribute('data-theme', selectedScheme);
+        // Apply the selected color scheme with a transition effect
+        // root.style.transition = 'background-color 0.5s';
+        root.setAttribute('data-theme', selectedScheme);
 
-    // Remove transition after it's applied to prevent it from affecting subsequent changes
-    // setTimeout(() => {
-    //   root.style.transition = '';
-    // }, 500);
-  }
-
-  colorSchemeInput.forEach(input => {
-    input.addEventListener('change', applyColorScheme);
-  });
-
-  // Check if the userColorScheme value is nonexistent, create one with the default value 'auto'
-  const userColorScheme = localStorage.getItem('userColorScheme');
-  if (!userColorScheme) {
-    localStorage.setItem('userColorScheme', 'auto');
-  }
-
-  // Check the initial color scheme preference
-  const initialColorScheme = userColorScheme || 'auto';
-  console.log(initialColorScheme);
-  document.querySelector(`input[value="${initialColorScheme}"]`).checked = true;
-  applyColorScheme(); // Apply initial color scheme
-
-
-  // Language
-  langOptionInput.forEach(input => {
-    input.addEventListener('change', setLanguage);
-  });
-
-  // Check the initial language
-  const initialLanguage = userLanguage || 'ja';
-  document.querySelector(`input[value="${initialLanguage}"]`).checked = true;
-
-  function setTranslationOptions(e) {
-    const currentOption = e.target;
-    const currentOptionId = e.target.id;
-    const currentOptionValue = e.target.checked;
-    const translationOptionsWrapper = document.querySelector('#site-menu .menu-list-item.translation-options');
-
-    if (e) {
-      translationOptionsWrapper.classList.add('setting-changed');
+        // Remove transition after it's applied to prevent it from affecting subsequent changes
+        // setTimeout(() => {
+        //   root.style.transition = '';
+        // }, 500);
     }
 
-    // Save the selected language in localStorage
-    localStorage.setItem(`userOption_${currentOptionId}`, currentOptionValue);
-  }
+    colorSchemeInput.forEach(input => {
+        input.addEventListener('change', applyColorScheme);
+    });
 
-  // Translate Character Names
-  translationOptions.forEach(input => {
-    input.addEventListener('change', setTranslationOptions);
-  });
+    // Check if the userColorScheme value is nonexistent, create one with the default value 'auto'
+    const userColorScheme = localStorage.getItem('userColorScheme');
+    if (!userColorScheme) {
+        localStorage.setItem('userColorScheme', 'auto');
+    }
 
-  // Check the initial language
-  // const initialTranslationOption = userLanguage || 'ja';
-  // document.querySelector(`input[value="${initialLanguage}"]`).checked = true;
+    // Check the initial color scheme preference
+    const initialColorScheme = userColorScheme || 'auto';
+    console.log(initialColorScheme);
+    document.querySelector(`input[value="${initialColorScheme}"]`).checked = true;
+    applyColorScheme(); // Apply initial color scheme
+
+
+    // Language
+    langOptionInput.forEach(input => {
+        input.addEventListener('change', setLanguage);
+    });
+
+    // Check the initial language
+    const initialLanguage = userLanguage || 'ja';
+    document.querySelector(`input[value="${initialLanguage}"]`).checked = true;
+
+    function setTranslationOptions(e) {
+        const currentOption = e.target;
+        const currentOptionId = e.target.id;
+        const currentOptionValue = e.target.checked;
+        const translationOptionsWrapper = document.querySelector('#site-menu .menu-list-item.translation-options');
+
+        if (e) {
+            translationOptionsWrapper.classList.add('setting-changed');
+        }
+
+        // Save the selected language in localStorage
+        localStorage.setItem(`userOption_${currentOptionId}`, currentOptionValue);
+    }
+
+    // Translate Character Names
+    translationOptions.forEach(input => {
+        input.addEventListener('change', setTranslationOptions);
+    });
+
+    // Check the initial language
+    // const initialTranslationOption = userLanguage || 'ja';
+    // document.querySelector(`input[value="${initialLanguage}"]`).checked = true;
 });
 
 
 
 // Determine appropriate transitionEvent for current browser
 const getTransitionEvent = () => {
-  const el = document.createElement("fakeelement");
+    const el = document.createElement("fakeelement");
 
-  const transitions = {
-    "transition"      : "transitionend",
-    "OTransition"     : "oTransitionEnd",
-    "MozTransition"   : "transitionend",
-    "WebkitTransition": "webkitTransitionEnd"
-  };
+    const transitions = {
+        "transition": "transitionend",
+        "OTransition": "oTransitionEnd",
+        "MozTransition": "transitionend",
+        "WebkitTransition": "webkitTransitionEnd"
+    };
 
-  for (const property in transitions){
-    if (el.style[property] !== undefined){
-      return transitions[property];
+    for (const property in transitions) {
+        if (el.style[property] !== undefined) {
+            return transitions[property];
+        }
     }
-  }
 };
 
 const transitionEvent = getTransitionEvent();
@@ -122,88 +122,91 @@ const transitionEvent = getTransitionEvent();
 // themeSettingRadio.addEventListener('change', switchTheme, false);
 
 transitionEndCallback = (e) => {
-  root.removeEventListener(transitionEvent, transitionEndCallback);
-  root.classList.remove('transitioning');
+    root.removeEventListener(transitionEvent, transitionEndCallback);
+    root.classList.remove('transitioning');
 }
 
 function updateQueryStringParameter(param, val) {
-  var searchParams = new URLSearchParams(window.location.search);
+    var searchParams = new URLSearchParams(window.location.search);
 
-  if ('URLSearchParams' in window) {
-    if (val === "") {
-        searchParams.delete(param);
-    } else {
-        searchParams.set(param, val);
+    if ('URLSearchParams' in window) {
+        if (val === "") {
+            searchParams.delete(param);
+        } else {
+            searchParams.set(param, val);
+        }
+        var newRelativePathQuery = window.location.pathname + '?' + searchParams.toString();
+        history.pushState(null, '', newRelativePathQuery);
     }
-    var newRelativePathQuery = window.location.pathname + '?' + searchParams.toString();
-    history.pushState(null, '', newRelativePathQuery);
-  }
 }
 
 function clearQueryStringParameter(excludeChartLevel) {
-  var searchParams = new URLSearchParams(window.location.search);
+    var searchParams = new URLSearchParams(window.location.search);
 
-  // Check if 'URLSearchParams' is supported by the browser
-  if ('URLSearchParams' in window) {
-    // Get the current path without the query string
-    var newRelativePathQuery = window.location.pathname;
+    // Check if 'URLSearchParams' is supported by the browser
+    if ('URLSearchParams' in window) {
+        // Get the current path without the query string
+        var newRelativePathQuery = window.location.pathname;
 
-    // If excludeChartLevel is true, remove all parameters except 'chart_lev'
-    if (excludeChartLevel) {
-      var chartLevValue = searchParams.get('chart_lev');
-      searchParams = new URLSearchParams(); // Clear all parameters
+        // If excludeChartLevel is true, remove all parameters except 'chart_lev'
+        if (excludeChartLevel) {
+            var chartLevValue = searchParams.get('chart_lev');
+            searchParams = new URLSearchParams(); // Clear all parameters
 
-      // Add back 'chart_lev' parameter if it exists
-      if (chartLevValue !== null) {
-        searchParams.set('chart_lev', chartLevValue);
-      }
-    } else {
-      // Clear all parameters
-      searchParams = new URLSearchParams();
+            // Add back 'chart_lev' parameter if it exists
+            if (chartLevValue !== null) {
+                searchParams.set('chart_lev', chartLevValue);
+            }
+        } else {
+            // Clear all parameters
+            searchParams = new URLSearchParams();
+        }
+
+        // Update the URL with the new path and modified query string
+        history.pushState(null, '', newRelativePathQuery + (searchParams.toString() ? '?' + searchParams.toString() : ''));
     }
-
-    // Update the URL with the new path and modified query string
-    history.pushState(null, '', newRelativePathQuery + (searchParams.toString() ? '?' + searchParams.toString() : ''));
-  }
 }
 
 function appendSelectboxStateClass(select, val) {
-  if (val !== "") {
-    select.addClass('changed');
-  } else {
-    select.removeClass('changed');
-  }
+    if (val !== "") {
+        select.addClass('changed');
+    } else {
+        select.removeClass('changed');
+    }
 }
 
 function replaceUnitText(obj) {
-  let item_unit;
-  switch (userLanguage) {
-    case 'ja':
-      item_unit = flat_view ? '譜面' : '曲';
-      break;
-    case 'ko':
-      item_unit = flat_view ? ' 보면' : ' 곡';
-      break;
-    case 'en':
-      item_unit = flat_view ? ' charts' : ' songs';
-      break;
-  }
-  // Check if the input is an object
-  if (obj !== null && typeof obj === 'object') {
-    // Iterate over each key-value pair in the object
-    for (const key in obj) {
-      if (obj.hasOwnProperty(key)) {
-        // Recursively call replaceUnitText for nested objects or arrays
-        obj[key] = replaceUnitText(obj[key]);
-
-        // Check if the value is a string and replace 'unit' if found
-        if (typeof obj[key] === 'string') {
-          obj[key] = obj[key].replace("{unit}", item_unit);
-        }
-      }
+    let item_unit;
+    switch (userLanguage) {
+        case 'ja':
+            item_unit = flat_view ? '譜面' : '曲';
+            break;
+        case 'ko':
+            item_unit = flat_view ? ' 보면' : ' 곡';
+            break;
+        case 'en':
+            item_unit = flat_view ? ' charts' : ' songs';
+            break;
+        case 'cn':
+            item_unit = flat_view ? ' 谱面' : ' 乐曲';
+            break;
     }
-  }
-  return obj;
+    // Check if the input is an object
+    if (obj !== null && typeof obj === 'object') {
+        // Iterate over each key-value pair in the object
+        for (const key in obj) {
+            if (obj.hasOwnProperty(key)) {
+                // Recursively call replaceUnitText for nested objects or arrays
+                obj[key] = replaceUnitText(obj[key]);
+
+                // Check if the value is a string and replace 'unit' if found
+                if (typeof obj[key] === 'string') {
+                    obj[key] = obj[key].replace("{unit}", item_unit);
+                }
+            }
+        }
+    }
+    return obj;
 }
 
 
@@ -217,112 +220,112 @@ function replaceUnitText(obj) {
 // });
 
 
-$(document).ready(function() {
-  if ('URLSearchParams' in window) {
-    updateChartLevelSelectboxValue(searchParams);
-  }
-  $('html').removeClass('page-loading');
-
-  // fetch('https://api.github.com/repos/zvuc/ongeki-db/commits?per_page=1')
-  //     .then(res => res.json())
-  //     .then(res => {
-  //         let commitDateTime = new Date(res[0].commit.committer.date);
-  //         let commitMsg = res[0].commit.message;
-  //         // if multi-line commit message
-  //         if(commitMsg.split('\n')[1] !== undefined) {
-  //             document.getElementById('latest-commit-content').innerHTML = commitMsg.split('\n').slice(2).join('<br>');
-  //         } else {
-  //             document.getElementById('latest-commit-content').innerHTML = commitMsg;
-  //         }
-  //         document.getElementById('latest-commit').setAttribute('href', res[0].html_url);
-  //         document.getElementById('latest-commit-date').innerHTML = commitDateTime.toISOString().split('T')[0]
-  // })
-
-  // localStorage.noticeNewAddress == "false" ? '' : $('.notice-wrap').addClass('visible');
-
-  // google analytics
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-141271073-1');
-  gtag('config', 'G-YZ8GJR7QFL');
-
-
-  // Site Menu
-  siteMenuBtn.on("click", function () {
-    event.preventDefault();
-    if (!isMenuOpen) {
-      siteMenuBtn.addClass('active');
-      openMenu();
-    } else {
-      closeMenu();
+$(document).ready(function () {
+    if ('URLSearchParams' in window) {
+        updateChartLevelSelectboxValue(searchParams);
     }
-  });
+    $('html').removeClass('page-loading');
 
-  $(document).on("keydown", function (e) {
-    if (e.key === "Escape" && isMenuOpen) {
-      closeMenu();
-    }
-  });
+    // fetch('https://api.github.com/repos/zvuc/ongeki-db/commits?per_page=1')
+    //     .then(res => res.json())
+    //     .then(res => {
+    //         let commitDateTime = new Date(res[0].commit.committer.date);
+    //         let commitMsg = res[0].commit.message;
+    //         // if multi-line commit message
+    //         if(commitMsg.split('\n')[1] !== undefined) {
+    //             document.getElementById('latest-commit-content').innerHTML = commitMsg.split('\n').slice(2).join('<br>');
+    //         } else {
+    //             document.getElementById('latest-commit-content').innerHTML = commitMsg;
+    //         }
+    //         document.getElementById('latest-commit').setAttribute('href', res[0].html_url);
+    //         document.getElementById('latest-commit-date').innerHTML = commitDateTime.toISOString().split('T')[0]
+    // })
 
-  $("#site-menu-backdrop").on("click", function () {
-    if (isMenuOpen) {
-      closeMenu();
-    }
-  });
-  $("#site-menu-close-btn").on("click", function () {
-    if (isMenuOpen) {
-      closeMenu();
-    }
-  });
+    // localStorage.noticeNewAddress == "false" ? '' : $('.notice-wrap').addClass('visible');
 
-  function openMenu() {
-    scrollPos = $(window).scrollTop();
-    $("#app-wrapper").css('top', (scrollPos * -1)).addClass("menu-open");
-    $(window).scrollTop(0);
+    // google analytics
+    window.dataLayer = window.dataLayer || [];
+    function gtag() { dataLayer.push(arguments); }
+    gtag('js', new Date());
+
+    gtag('config', 'UA-141271073-1');
+    gtag('config', 'G-YZ8GJR7QFL');
 
 
-    siteMenuWrap
-      .addClass('open')
-      .addClass('anim-enter')
-      .one('animationend webkitAnimationEnd oAnimationEnd', function () {
-
-      event.stopPropagation();
-      siteMenuWrap.removeClass('anim-enter');
-      isMenuOpen = true;
+    // Site Menu
+    siteMenuBtn.on("click", function () {
+        event.preventDefault();
+        if (!isMenuOpen) {
+            siteMenuBtn.addClass('active');
+            openMenu();
+        } else {
+            closeMenu();
+        }
     });
 
-  }
-
-  function closeMenu() {
-    siteMenuWrap
-      .addClass('anim-leave')
-      .one('animationend webkitAnimationEnd oAnimationEnd', function () {
-
-      siteMenuWrap.removeClass('open').removeClass('anim-leave');
+    $(document).on("keydown", function (e) {
+        if (e.key === "Escape" && isMenuOpen) {
+            closeMenu();
+        }
     });
 
-    $("#app-wrapper").removeClass("menu-open").css('top', '');
-    $(window).scrollTop(scrollPos);
+    $("#site-menu-backdrop").on("click", function () {
+        if (isMenuOpen) {
+            closeMenu();
+        }
+    });
+    $("#site-menu-close-btn").on("click", function () {
+        if (isMenuOpen) {
+            closeMenu();
+        }
+    });
 
-    isMenuOpen = false;
-    siteMenuBtn.removeClass('active');
-  }
+    function openMenu() {
+        scrollPos = $(window).scrollTop();
+        $("#app-wrapper").css('top', (scrollPos * -1)).addClass("menu-open");
+        $(window).scrollTop(0);
 
-  // $(document).click(function(event) {
-  //   console.log('clicked')
-  //   var $target = $(event.target);
-  //   if (!$target.closest('#site-menu-btn').length &&
-  //     !$target.closest('#site-menu-wrap').length &&
-  //     isMenuOpen) {
 
-  //     closeMenu();
-  //   }
-  // });
+        siteMenuWrap
+            .addClass('open')
+            .addClass('anim-enter')
+            .one('animationend webkitAnimationEnd oAnimationEnd', function () {
 
-  $('#table').on( 'page.dt', function () {
-    $('html').scrollTop(0);
-  });
+                event.stopPropagation();
+                siteMenuWrap.removeClass('anim-enter');
+                isMenuOpen = true;
+            });
+
+    }
+
+    function closeMenu() {
+        siteMenuWrap
+            .addClass('anim-leave')
+            .one('animationend webkitAnimationEnd oAnimationEnd', function () {
+
+                siteMenuWrap.removeClass('open').removeClass('anim-leave');
+            });
+
+        $("#app-wrapper").removeClass("menu-open").css('top', '');
+        $(window).scrollTop(scrollPos);
+
+        isMenuOpen = false;
+        siteMenuBtn.removeClass('active');
+    }
+
+    // $(document).click(function(event) {
+    //   console.log('clicked')
+    //   var $target = $(event.target);
+    //   if (!$target.closest('#site-menu-btn').length &&
+    //     !$target.closest('#site-menu-wrap').length &&
+    //     isMenuOpen) {
+
+    //     closeMenu();
+    //   }
+    // });
+
+    $('#table').on('page.dt', function () {
+        $('html').scrollTop(0);
+    });
 
 });
